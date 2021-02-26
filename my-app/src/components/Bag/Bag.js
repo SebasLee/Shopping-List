6d01dd8619f669fa336cd1ee5aec19c7a1ef0687
SebasLee <heechanlee98@gmail.com>
2021-02-25 16:35:55 -0800

    switchups

--- my-app/src/components/Bag/Bag.js
+++ my-app/src/components/Bag/Bag.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const Bag = (props) => {
-    return(
-        <div>
-            <h1>{props.message}</h1>
-        </div>    
-    );
-};
+        return (
+            <div>
+                <h1>{props.message}</h1>
+                <h2>Hello</h2>
+            </div>
+        )
+}
 
 export default Bag;
--- my-app/src/components/Selection/Selection.js
+++ my-app/src/components/Selection/Selection.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Bag from '../Bag/Bag';
 
-class Selection extends React.Component {
-    render() {
-        return (
-            <div>
-                <Bag message="test"/>
-            </div>
-        )
-    }
-}
+const Selection = (props) => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <Bag message="test" />
+        </div>
+    )   
+};
 
 export default Selection;
